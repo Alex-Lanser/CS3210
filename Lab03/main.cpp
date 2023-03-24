736971ddf0465929cb8d98b03ba8a317e5a7abc4
Alex-Lanser <alanser@icloud.com>
2023-03-24 17:19:21 -0500

    More main tests

--- Lab03/main.cpp
+++ Lab03/main.cpp
@@ -25,14 +25,14 @@ int main()
 
 	cout << "Matrix 2" << endl;
 	num = 8;
-	for (int i = 2; i >= 0; i--)
+	for (int i = 0; i < 3; i++)
 	{
 		cout << "[ ";
-		for (int j = 2; j >= 0; j--)
+		for (int j = 0; j < 3; j++)
 		{
 			matrix2[i][j] = num;
-			num -= 1;
 			cout << matrix2[i][j] << " ";
+			num -= 1;
 		}
 		cout << "]" << endl;
 	}
@@ -81,7 +81,56 @@ int main()
 	cout << endl;
 
 	cout << "Add 2 different size matrices" << endl;
-	matrixAdd = matrix1 + matrix3;
+	try
+	{
+		matrixAdd = matrix1 + matrix3;
+	}
+	catch(...)
+	{
+		cout << "Caught adding 2 different sized matrices." << endl << endl;
+	}
+
+	cout << "Multiply 2 same size matrices" << endl;
+	Matrix matrixMult(3, 3);
+	matrixMult = matrix1 * matrix2;
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "[ ";
+		for (int j = 0; j < 3; j++)
+		{
+			cout << matrixMult[i][j] << " ";
+		}
+		cout << "]" << endl;
+	}
+	cout << endl;
+
+	cout << "Multiply scalar multiple. Matrix * scalar" << endl;
+	Matrix scalarMult1(3, 3);
+	scalarMult1 = matrix1 * 4;
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "[ ";
+		for (int j = 0; j < 3; j++)
+		{
+			cout << scalarMult1[i][j] << " ";
+		}
+		cout << "]" << endl;
+	}
+	cout << endl;
+
+	cout << "Multiply scalar multiple. Scalar * matrix" << endl;
+	Matrix scalarMult2(3, 3);
+	scalarMult2 = 4 * matrix1;
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "[ ";
+		for (int j = 0; j < 3; j++)
+		{
+			cout << scalarMult2[i][j] << " ";
+		}
+		cout << "]" << endl;
+	}
+	cout << endl;
 
 	return 0;
 }
