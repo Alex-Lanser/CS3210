5fe672d11eed4ad5327fb96800951cd64e3f43c0
Alex-Lanser <alanser@icloud.com>
2023-03-30 22:47:00 -0500

    Inheritance notes

--- Notes/Inheritance/main.cpp
+++ Notes/Inheritance/main.cpp
@@ -7,7 +7,12 @@ public:
     string name;
     Pet();
     Pet(string name, int num_legs);
-    void print_type();
+    virtual void print_type();
+    // virtual void print_type() = 0; // When = 0, it is now a pure virtual function. Means that the child class must have this function
+    // Also makes the class abstract
+
+    ~Pet();
+
 private:
     int num_legs;
 };
@@ -24,6 +29,11 @@ Pet::Pet(string name, int num_legs)
     cout << "Pet parameterized constructor" << endl;
 }
 
+Pet::~Pet()
+{
+    cout << "In pet destructor" << endl;
+}
+
 void Pet::print_type()
 {
     cout << "Type is Pet" << endl;
@@ -35,6 +45,8 @@ public:
     int fish;
     Cat();
     Cat(string name, int fish);
+    void print_type();
+    ~Cat();
 };
 
 Cat::Cat()
@@ -51,6 +63,15 @@ Cat::Cat(string name, int fish) : Pet(name, 4), fish(fish)
     cout << "Cat parameterized constructor" << endl;
 }
 
+void Cat::print_type()
+{
+    cout << "Type is Cat" << endl;
+}
+
+Cat::~Cat()
+{
+    cout << "In cat destructor" << endl;
+}
 int main()
 {
     Pet p("Spot", 4);
@@ -66,6 +87,31 @@ int main()
     Pet *petptr1 = &p;
     Cat *catptr1 = &c;
     Pet *petptr2 = &c; // This works because cat is a pet
+    petptr2->print_type();
+    // Child class may override a virtual function
+    // But it does not have to
+
+    Pet *petptr3 = new Cat();
+    delete petptr3;
+
+    Pet *pets[3];
+    pets[0] = new Cat("Tom", 10);
+    pets[1] = new Cat("Jerry", 3);
+    pets[2] = new Cat("Whiskers", 6); // Can put whatever pet into here if there are more pets... Like fish
+
+    Pet *petsCopy[3];
+    for (int i = 0; i < 3; i++)
+    {
+        petsCopy[i] = pets[i]; // Shallow copy
+    }
+
+
+    for (int i = 0; i < 3; i++)
+    {
+        delete pets[i];
+    }
+    // delete[] pets <-- Do not need since it is on the stack. "Pet* pets[3];"
+
     // Cat *catptr2 = &p; // This will not work because a child cannot point to a parent
     // Pet pet1 = c; // This is slicing. Try to avoid. There's no room in pet1 for the c fish.
     // Will only keep the pet part. Removes the things that makes cat unique from pets.
