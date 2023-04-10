777ba1b26bb005ada6b4ffe694d3be0d2a3525ab
Alex-Lanser <alanser@icloud.com>
2023-04-10 14:55:28 -0500

    Finished lab 4

--- Lab04/main.cpp
+++ Lab04/main.cpp
@@ -100,8 +100,60 @@ int main(void)
 		gc->drawLine(400, 400, 500, 290); // Right Arm
 		sleep(1);
 	}
-	sleep(1);
 
+	while (i <= 400)
+	{
+		gc->setColor(GraphicsContext::BLACK); // Reset previous arm frame
+		gc->drawLine(400, 400, 500, i - 10);
+		gc->setColor(GraphicsContext::GREEN);
+		gc->drawLine(400, 400, 500, i); // Right Arm
+		i = i + 10;
+		sleep(1);
+	}
+
+	gc->setColor(GraphicsContext::BLACK);
+	gc->drawLine(353, 243, 352, 242);
+	gc->drawLine(447, 243, 448, 242);
+	sleep(1);
+	gc->drawLine(355, 245, 354, 244);
+	gc->drawLine(445, 245, 446, 244);
+	sleep(1);
+	gc->drawLine(357, 247, 356, 246);
+	gc->drawLine(443, 247, 444, 246);
+	sleep(1);
+	gc->drawLine(359, 249, 358, 248);
+	gc->drawLine(441, 249, 442, 248);
+
+	gc->setColor(GraphicsContext::RED);
+	gc->drawLine(359, 251, 358, 252);
+	gc->drawLine(440, 251, 441, 252);
+	sleep(1);
+	gc->drawLine(357, 253, 356, 254);
+	gc->drawLine(442, 253, 443, 254);
+	sleep(1);
+	gc->drawLine(355, 255, 354, 256);
+	gc->drawLine(444, 255, 445, 256);
+	sleep(1);
+	gc->drawLine(353, 257, 352, 258);
+	gc->drawLine(446, 257, 447, 258);
+	gc->setColor(GraphicsContext::BLUE);
+	gc->drawLine(355, 185, 355, 188);
+	sleep(1);
+	gc->setColor(GraphicsContext::BLACK);
+	gc->drawLine(355, 185, 355, 188);
+	gc->setColor(GraphicsContext::BLUE);
+	gc->drawLine(355, 188, 355, 191);
+	sleep(1);
+	gc->setColor(GraphicsContext::BLACK);
+	gc->drawLine(355, 188, 355, 191);
+	gc->setColor(GraphicsContext::BLUE);
+	gc->drawLine(355, 191, 355, 194);
+	sleep(1);
+	gc->setColor(GraphicsContext::BLACK);
+	gc->drawLine(355, 191, 355, 194);
+	gc->setColor(GraphicsContext::BLUE);
+	gc->drawLine(355, 194, 355, 197);
+	sleep(1);
 	delete gc;
 
 	return 0;
