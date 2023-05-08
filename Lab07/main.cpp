ef35cf7d3718246b14445307de2adaee562ff9cb
Alex-Lanser <alanser@icloud.com>
2023-05-08 14:11:23 -0500

    Changed window size

--- Lab07/main.cpp
+++ Lab07/main.cpp
@@ -5,8 +5,8 @@
 #include <cstring>
 int main(void)
 {
-    GraphicsContext *gc = new X11Context(1910, 1200, GraphicsContext::BLACK);
-    // GraphicsContext *gc = new X11Context(800, 600, GraphicsContext::BLACK);
+    // GraphicsContext *gc = new X11Context(1910, 1200, GraphicsContext::BLACK);
+    GraphicsContext *gc = new X11Context(1000, 800, GraphicsContext::BLACK);
     gc->setColor(GraphicsContext::GREEN);
     // make a drawing
     MyDrawing md(gc->getWindowWidth(), gc->getWindowHeight());
